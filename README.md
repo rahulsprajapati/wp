c523adfdfb000fc280d15911578751b976c2f506
Rahul Prajapati <rahul.prajapati@live.in>
2019-01-03 16:18:12 +0530

    Add wordpress docker-compose template

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# wp
+Run command:
+
+docker-compose up -d
+
+# wp cli 
+
+docker-compose exec cli wp cli info
---
+++ plugins/test_plugins.php
@@ -0,0 +1,10 @@
+/**
+ * Plugin Name: Test plugins
+ * Plugin URI: testplugins.test
+ * Description: Plugins description...
+ * Author: testauthor
+ * Version: 1.0.0
+ * Author URI: test.author
+ */
+
+
